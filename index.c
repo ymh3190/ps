0b00e7cf15e6b048b3bab8e35b42c3932cb79f99
ymh3190 <ymh3190@gmail.com>
2025-02-10 22:44:32 +0900

    clear

--- index.c
+++ index.c
@@ -1,29 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#include "quickSort3.c"
 
 int main() {
-  FILE* fptr;
-  fptr = fopen("input.txt", "r");
+  FILE* fd;
+  fd = fopen("input.txt", "r");
 
   int n;
-  fscanf(fptr, "%d", &n);
+  fscanf(fd, "%d", &n);
 
-  str arrs[n];
-  for (int i = 0; i < n; i++) {
-    fscanf(fptr, "%s", arrs[i].s);
-    arrs[i].len = strlen(arrs[i].s);
-  }
-
-  qsort(arrs, n, sizeof(str), compare);
-  for (int i = 0; i < n; i++) {
-    if (strcmp(arrs[i + 1].s, arrs[i].s) || i == n - 1) {
-      printf("%s\n", arrs[i].s);
-    }
-  }
-
-  fclose(fptr);
+  fclose(fd);
   return 0;
 }
