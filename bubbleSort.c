20d8a1e44e3c41a34c80ad8294818ca05a3b3df3
ymh3190 <ymh3190@gmail.com>
2025-02-10 17:37:06 +0900

    bubble sort 수정

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,18 +1,14 @@
-void swap(int* xp, int* yp) {
-  int tmp = *xp;
-  *xp = *yp;
-  *yp = tmp;
-}
-
 /**
  * @param arrs input array
  * @param n last index
  */
 void bubbleSort(int arrs[], int n) {
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n - 1; i++) {
     for (int j = 0; j < n - i - 1; j++) {
       if (arrs[j + 1] < arrs[j]) {
-        swap(&arrs[j], &arrs[j + 1]);
+        int t = arrs[j + 1];
+        arrs[j + 1] = arrs[j];
+        arrs[j] = t;
       }
     }
   }
