44419f963023fef94d44016a8cc62d59b6090bb3
ymh3190 <ymh3190@gmail.com>
2025-02-11 21:01:43 +0900

    10814 node

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let fs = require("fs");
-let [_, ...input] = fs.readFileSync("input.txt").toString().trim().split("\n");
+let input = fs.readFileSync("input.txt").toString().trim().split("\n");
+let n = Number(input.shift());
 let arrs = input
-  .map((v) => v.split(" ").map(Number))
+  .map((v) => v.split(" "))
+  .map((v, i) => {
+    v[0] = Number(v[0]);
+    return { i, age: v[0], name: v[1] };
+  })
   .sort((a, b) => {
-    if (a[0] - b[0] < 0 || (a[0] == b[0] && a[1] < b[1])) return -1;
+    if (a.age < b.age) return -1;
+    else if (a.age == b.age && a.i < b.i) return -1;
     return 0;
   });
-for (const arr of arrs) {
-  console.log(arr.join(" "));
+
+for (let i = 0; i < n; i++) {
+  console.log(arrs[i].age, arrs[i].name);
 }
