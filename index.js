005686047de016dadebc67b50b90a74d9dfca71d
ymh3190 <ymh3190@gmail.com>
2025-02-11 19:44:12 +0900

    node 추가

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let fs = require("fs");
-let input = fs.readFileSync("input.txt").toString().trim().split("\n");
+let [_, ...input] = fs.readFileSync("input.txt").toString().trim().split("\n");
 let arrs = input
-  .pop()
-  .split(" ")
-  .map(Number)
-  .sort((a, b) => b - a);
-console.log(arrs);
+  .map((v) => v.split(" ").map(Number))
+  .sort((a, b) => {
+    if (a[0] - b[0] < 0 || (a[0] == b[0] && a[1] < b[1])) return -1;
+    return 0;
+  });
+for (const arr of arrs) {
+  console.log(arr.join(" "));
+}
