ed9e99a3ed0dcc9cec3db5c7ae3c791b9048ee01
ymh3190 <ymh3190@gmail.com>
2025-11-20 13:00:31 +0900

    Chore: -

--- index.c
+++ index.c
@@ -3,11 +3,9 @@
 int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
-
   int a, b;
   fscanf(fd, "%d %d", &a, &b);
-  printf("%d\n", a - b);
-
+  printf("%d\n", a * b);
   fclose(fd);
   return 0;
 }
