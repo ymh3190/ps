c8e8e5d2b07e7785b3402090fa68f03d0a19eb6b
ymh3190 <ymh3190@gmail.com>
2025-11-22 23:43:21 +0900

    Chore: -

--- index.c
+++ index.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-short f1(short* a, short* b, short* c) { return (*a + *b) % *c; }
-short f2(short* a, short* b, short* c) { return ((*a % *c) + (*b % *c)) % *c; }
-short f3(short* a, short* b, short* c) { return (*a * *b) % *c; }
-short f4(short* a, short* b, short* c) { return ((*a % *c) * (*b % *c)) % *c; }
-
 int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
   short a, b, c;
   fscanf(fd, "%hd %hd %hd", &a, &b, &c);
-  printf("%hd\n%hd\n%hd\n%hd\n", f1(&a, &b, &c), f2(&a, &b, &c), f3(&a, &b, &c),
-         f4(&a, &b, &c));
+  short arrs[4];
+  *arrs = (a + b) % c;
+  *(arrs + 1) = ((a % c) + (b % c)) % c;
+  *(arrs + 2) = (a * b) % c;
+  *(arrs + 3) = ((a % c) * (b % c)) % c;
+  for (int i = 0; i < 4; i++) {
+    printf("%hd\n", *(arrs + i));
+  }
+
   fclose(fd);
   return 0;
 }
