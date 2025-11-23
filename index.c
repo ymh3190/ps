62b91dab613eca7b521a15d34b4e2e834566332b
ymh3190 <ymh3190@gmail.com>
2025-11-23 23:18:35 +0900

    Chore: -

--- index.c
+++ index.c
@@ -6,10 +6,14 @@ int main() {
   short a, b, c;
   fscanf(fd, "%hd %hd %hd", &a, &b, &c);
   short arrs[4];
-  *arrs = (a + b) % c;
-  *(arrs + 1) = ((a % c) + (b % c)) % c;
-  *(arrs + 2) = (a * b) % c;
-  *(arrs + 3) = ((a % c) * (b % c)) % c;
+  // *arrs = (a + b) % c;
+  // *(arrs + 1) = ((a % c) + (b % c)) % c;
+  // *(arrs + 2) = (a * b) % c;
+  // *(arrs + 3) = ((a % c) * (b % c)) % c;
+  arrs[0] = (a + b) % c;
+  arrs[1] = ((a % c) + (b % c)) % c;
+  arrs[2] = (a * b) % c;
+  arrs[3] = ((a % c) * (b % c)) % c;
   for (int i = 0; i < 4; i++) {
     printf("%hd\n", *(arrs + i));
   }
