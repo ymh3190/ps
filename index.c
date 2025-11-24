300a7d7c190c11d1fc00bb9f227c3a170a0454c6
ymh3190 <ymh3190@gmail.com>
2025-11-24 17:26:43 +0900

    Chore: -

--- index.c
+++ index.c
@@ -3,21 +3,15 @@
 int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
-  short a, b, c;
-  fscanf(fd, "%hd %hd %hd", &a, &b, &c);
-  short arrs[4];
-  // *arrs = (a + b) % c;
-  // *(arrs + 1) = ((a % c) + (b % c)) % c;
-  // *(arrs + 2) = (a * b) % c;
-  // *(arrs + 3) = ((a % c) * (b % c)) % c;
-  arrs[0] = (a + b) % c;
-  arrs[1] = ((a % c) + (b % c)) % c;
-  arrs[2] = (a * b) % c;
-  arrs[3] = ((a % c) * (b % c)) % c;
-  for (int i = 0; i < 4; i++) {
-    printf("%hd\n", *(arrs + i));
-  }
 
+  int x, y;
+  fscanf(fd, "%d\n%d", &x, &y);
+
+  int a = y % 10;
+  int b = (y / 10) % 10;
+  int c = (y / 100);
+  printf("%d\n%d\n%d\n%d\n", x * a, x * b, x * c,
+         x * a + 10 * (x * b) + 100 * (x * c));
   fclose(fd);
   return 0;
 }
