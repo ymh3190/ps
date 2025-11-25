fce8bf6d4de4f7a9da72085d79ce58b178f42512
ymh3190 <ymh3190@gmail.com>
2025-11-25 21:18:33 +0900

    Chore: -

--- index.c
+++ index.c
@@ -1,13 +1,37 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  char x;
-  fscanf(fd, "%d", &x);
-  printf("%d", x);
-
+  short x, y;
+  fscanf(fd, "%hd\n%hd", &x, &y);
   fclose(fd);
-  return 0;
+
+  bool isQ1 = x > 0 && y > 0;
+  if (isQ1) {
+    printf("1");
+    return 0;
+  }
+
+  bool isQ2 = x < 0 && y > 0;
+  if (isQ2) {
+    printf("2");
+    return 0;
+  }
+
+  bool isQ3 = x < 0 && y < 0;
+  if (isQ3) {
+    printf("3");
+    return 0;
+  }
+
+  bool isQ4 = x > 0 && y < 0;
+  if (isQ4) {
+    printf("4");
+    return 0;
+  }
+
+  return 1;
 }
