5b13b29c8c35250c2e1ff1cf91cfa496735629d3
ymh3190 <ymh3190@gmail.com>
2025-11-25 14:54:40 +0900

    Chore: -

--- index.c
+++ index.c
@@ -4,14 +4,10 @@ int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  int x, y;
-  fscanf(fd, "%d\n%d", &x, &y);
+  long long a, b, c;
+  fscanf(fd, "%ld %ld %ld", &a, &b, &c);
+  printf("%ld", a + b + c);
 
-  int a = y % 10;
-  int b = (y / 10) % 10;
-  int c = (y / 100);
-  printf("%d\n%d\n%d\n%d\n", x * a, x * b, x * c,
-         x * a + 10 * (x * b) + 100 * (x * c));
   fclose(fd);
   return 0;
 }
