f17f0dc05fa9e6a9e0b0e816985adc8127d80ef8
ymh3190 <ymh3190@gmail.com>
2025-11-27 18:32:04 +0900

    Chore

--- index.c
+++ index.c
@@ -4,15 +4,9 @@ int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  char n;
-  fscanf(fd, "%hhd", &n);
-  fclose(fd);
-
-  char arrs[9][11];
-  for (char i = 0; i < 9; i++) {
-    sprintf(arrs[i], "%hhd * %hhd = %hhd", n, i + 1, n * (i + 1));
-    printf("%s\n", arrs[i]);
-  }
+  short n;
+  fscanf(fd, "%hd", &n);
+  printf("%d", n * (n + 1) / 2);
 
   return 0;
 }
