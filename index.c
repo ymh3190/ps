2839fc05aa461a7262af50ad0f0f6709e8e9586c
ymh3190 <ymh3190@gmail.com>
2025-11-28 23:30:18 +0900

    malloc

--- index.c
+++ index.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// option 1
-// char buf[1000000][6];
+typedef struct {
+  int a;
+  int b;
+  int c;
+} Buf;
 
 int main() {
   FILE* fd;
@@ -10,18 +13,16 @@ int main() {
 
   int t;
   fscanf(fd, "%d", &t);
+  // Buf buf[t];
+  Buf* buf = malloc(sizeof(Buf) * t);
+  for (int i = 0; i < t; i++) {
+    fscanf(fd, "%d %d", &(buf[i].a), &(buf[i].b));
+    buf[i].c = buf[i].a + buf[i].b;
+  }
+  for (int i = 0; i < t; i++) {
+    printf("Case #%d: %d + %d = %d\n", i + 1, buf[i].a, buf[i].b, buf[i].c);
+  }
 
-  // option 2
-  // static char buf[1000000][6];
-  char (*buf)[6] = (char (*)[6])malloc(sizeof(char) * 1000000 * 6);
-  int a, b;
-  for (int i = 0; i < t; i++) {
-    fscanf(fd, "%d %d", &a, &b);
-    sprintf(buf[i], "%d\n", a + b);
-  }
-  for (int i = 0; i < t; i++) {
-    printf("%s", buf[i]);
-  }
   free(buf);
   fclose(fd);
   return 0;
