2b2ab73e302b86582eca4337c808a1f29eed49c6
ymh3190 <ymh3190@gmail.com>
2025-11-28 15:41:18 +0900

    Chore

--- README.md
+++ README.md
@@ -20,3 +20,9 @@ char[]
 
 char
 scanf("%hhd", ...);
+
+윈도우의 기본 스택 크기는 1MB
+char buf[1000000][6] => 6MB
+따라서 static 변수, 전역 변수, 힙 할당 중에서 선택해서 해결
+
+char (\*buf)[6] = (char (\*)[6])malloc(sizeof(char) \* 1000000 \* 6);
--- index.c
+++ index.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// option 1
+// char buf[1000000][6];
 
 int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  short n;
-  fscanf(fd, "%hd", &n);
-  printf("%d", n * (n + 1) / 2);
+  int t;
+  fscanf(fd, "%d", &t);
 
+  // option 2
+  // static char buf[1000000][6];
+  char (*buf)[6] = (char (*)[6])malloc(sizeof(char) * 1000000 * 6);
+  int a, b;
+  for (int i = 0; i < t; i++) {
+    fscanf(fd, "%d %d", &a, &b);
+    sprintf(buf[i], "%d\n", a + b);
+  }
+  for (int i = 0; i < t; i++) {
+    printf("%s", buf[i]);
+  }
+  free(buf);
+  fclose(fd);
   return 0;
 }
