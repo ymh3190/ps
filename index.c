982eebd4a730283fb816a05e480fab46f193d90f
ymh3190 <ymh3190@gmail.com>
2025-11-29 23:26:45 +0900

    1d

--- index.c
+++ index.c
@@ -4,11 +4,22 @@ int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  int a, b;
-  while (1) {
-    if (fscanf(fd, "%d %d", &a, &b) == EOF) break;
-    printf("%d\n", a + b);
+  int n;
+  fscanf(fd, "%d", &n);
+
+  int buf[n];
+  for (int i = 0; i < n; i++) {
+    fscanf(fd, "%d", &buf[i]);
   }
+
+  int c = 0;
+  int v;
+  fscanf(fd, "%d", &v);
+  for (int i = 0; i < n; i++) {
+    if (buf[i] == v) c++;
+  }
+  printf("%d", c);
+
   fclose(fd);
   return 0;
 }
