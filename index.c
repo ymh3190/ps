00c0b83f5dc0cbef1704f6d95e78f3d896549786
ymh3190 <ymh3190@gmail.com>
2025-11-30 12:09:54 +0900

    2562

--- index.c
+++ index.c
@@ -4,17 +4,23 @@ int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  int n, x;
-  fscanf(fd, "%d %d", &n, &x);
-
-  int buf[n];
-  for (int i = 0; i < n; i++) {
-    fscanf(fd, "%d", &buf[i]);
+  int buf[9];
+  int i = 0;
+  while (1) {
+    if (fscanf(fd, "%d", &buf[i]) == EOF) break;
+    i++;
   }
 
-  for (int i = 0; i < n; i++) {
-    if (buf[i] < x) printf("%d ", buf[i]);
+  int max = buf[0];
+  int idx = 0;
+  for (int i = 0; i < 9; i++) {
+    if (buf[i] > max) {
+      max = buf[i];
+      idx = i + 1;
+    }
   }
+
+  printf("%d\n%d", max, idx);
   fclose(fd);
   return 0;
 }
