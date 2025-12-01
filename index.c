300bcf5fe126f3dca781831a69b32f21105ac11c
ymh3190 <ymh3190@gmail.com>
2025-12-02 00:49:02 +0900

    11718

--- index.c
+++ index.c
@@ -4,19 +4,19 @@ int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  char buf[16];
-  fscanf(fd, "%s", buf);
-
-  int dials[26] = {3, 3, 3, 4, 4, 4, 5, 5, 5, 6,  6,  6,  7,
-                   7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
-
-  int s = 0;
-  for (int i = 0; i < 16; i++) {
-    if (buf[i] == '\0') break;
-    s += dials[buf[i] - 65];
+  char buf[100];
+  int i = 0;
+  while (1) {
+    if (fscanf(fd, "%c", &buf[i]) == EOF) break;
+    if (buf[i] == '\n') {
+      buf[i] = '\0';
+      i = 0;
+      printf("%s\n", buf);
+      continue;
+    }
+    i++;
   }
 
-  printf("%d", s);
   fclose(fd);
   return 0;
 }
