6b4bcbf60cc001407b2226bc8fd7736775e035ab
ymh3190 <ymh3190@gmail.com>
2025-12-01 11:10:23 +0900

    5597

--- index.c
+++ index.c
@@ -4,28 +4,26 @@ int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  int n;
-  int m;
-  fscanf(fd, "%d %d", &n, &m);
-
-  int buf[n];
-  for (int i = 0; i < n; i++) {
+  int buf[30];
+  for (int i = 0; i < 30; i++) {
     buf[i] = i + 1;
   }
 
-  for (int i = 0; i < m; i++) {
-    int a, b;
-    fscanf(fd, "%d %d", &a, &b);
-    a -= 1;
-    b -= 1;
-    int t = buf[a];
-    buf[a] = buf[b];
-    buf[b] = t;
+  for (int i = 0; i < 28; i++) {
+    int t;
+    fscanf(fd, "%d", &t);
+    for (int j = 0; j < 30; j++) {
+      if (buf[j] == t) {
+        buf[j] = 0;
+        break;
+      }
+    }
   }
 
-  for (int i = 0; i < n; i++) {
-    printf("%d ", buf[i]);
+  for (int i = 0; i < 30; i++) {
+    if (buf[i]) printf("%d ", buf[i]);
   }
+
   fclose(fd);
   return 0;
 }
