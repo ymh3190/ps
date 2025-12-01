0f948e4d27f40388fff947216da402d65fb53e89
ymh3190 <ymh3190@gmail.com>
2025-12-01 14:53:00 +0900

    1546

--- index.c
+++ index.c
@@ -5,31 +5,23 @@ int main() {
   fd = fopen("input.txt", "r");
 
   int n;
-  int m;
-  fscanf(fd, "%d %d", &n, &m);
+  fscanf(fd, "%d", &n);
 
   int buf[n];
   for (int i = 0; i < n; i++) {
-    buf[i] = i + 1;
+    fscanf(fd, "%d", &buf[i]);
   }
 
-  for (int i = 0; i < m; i++) {
-    int a;
-    int b;
-    fscanf(fd, "%d %d", &a, &b);
-    a -= 1;
-    b -= 1;
-    for (int j = a; j < b; j++) {
-      int t = buf[j];
-      buf[j] = buf[b];
-      buf[b] = t;
-      b--;
-    }
-  }
-
+  int m = buf[0];
   for (int i = 0; i < n; i++) {
-    printf("%d ", buf[i]);
+    if (buf[i] > m) m = buf[i];
   }
+
+  float sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += (float)buf[i] / m;
+  }
+  printf("%f", sum * 100 / n);
 
   fclose(fd);
   return 0;
