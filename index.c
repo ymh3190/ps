7c9423dde65bcf2a2ec3df3cb6371ef5b96a3c53
ymh3190 <ymh3190@gmail.com>
2025-12-01 22:43:53 +0900

    2908

--- index.c
+++ index.c
@@ -4,14 +4,15 @@ int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  char buf[1000000];
-  int c = 0;
-  while (1) {
-    if (fscanf(fd, "%s", buf) == EOF) break;
-    c++;
-  }
+  char buf_a[4];
+  char buf_b[4];
 
-  printf("%d", c);
+  fscanf(fd, "%s %s", buf_a, buf_b);
+  int a = (buf_a[2] - 48) * 100 + (buf_a[1] - 48) * 10 + (buf_a[0] - 48);
+  int b = (buf_b[2] - 48) * 100 + (buf_b[1] - 48) * 10 + (buf_b[0] - 48);
+
+  printf("%d", a > b ? a : b);
+
   fclose(fd);
   return 0;
 }
