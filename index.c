af5c03a25fc2d42e97acf11b5ceb1f47193b81e7
ymh3190 <ymh3190@gmail.com>
2025-12-02 21:34:39 +0900

    2444

--- index.c
+++ index.c
@@ -4,18 +4,54 @@ int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  char buf[100];
-  int i = 0;
-  while (1) {
-    if (fscanf(fd, "%c", &buf[i]) == EOF) break;
-    if (buf[i] == '\n') {
-      buf[i] = '\0';
-      i = 0;
-      printf("%s\n", buf);
-      continue;
+  int n;
+  fscanf(fd, "%d", &n);
+
+  int size = 2 * n - 1;
+  char buf[size + 1];
+
+  int mid = size / 2;
+  for (int i = 0; i <= mid; i++) {
+    for (int j = 0; j < mid - i; j++) {
+      buf[j] = ' ';
     }
-    i++;
+    buf[mid + i] = '*';
+    buf[mid - i] = '*';
+    buf[mid + i + 1] = '\0';
+    printf("%s\n", buf);
   }
+
+  for (int i = 0; i < mid; i++) {
+    buf[i] = ' ';
+    buf[size - 1 - i] = ' ';
+    buf[size - i] = '\0';
+    printf("%s\n", buf);
+  }
+
+  // 출력 형식 오류
+  // int size = 2 * n - 1;
+  // char buf[size];
+  // for (int i = 0; i < size; i++) {
+  //   buf[i] = ' ';
+  // }
+
+  // int mid = (size) / 2;
+  // for (int i = 0; i <= mid; i++) {
+  //   buf[mid - i] = '*';
+  //   buf[mid + i] = '*';
+  //   for (int j = 0; j < size; j++) {
+  //     printf("%c", buf[j]);
+  //   }
+  //   printf("\n");
+  // }
+  // for (int i = 0; i < mid; i++) {
+  //   buf[i] = ' ';
+  //   buf[size - 1 - i] = ' ';
+  //   for (int j = 0; j < size; j++) {
+  //     printf("%c", buf[j]);
+  //   }
+  //   printf("\n");
+  // }
 
   fclose(fd);
   return 0;
