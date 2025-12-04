1ae017b9abb70f1a5ef7c50c8f032dee73fe75a8
ymh3190 <ymh3190@gmail.com>
2025-12-04 22:51:37 +0900

    2745

--- index.c
+++ index.c
@@ -1,31 +1,30 @@
+#include <math.h>
 #include <stdio.h>
 
 int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  int n;
-  fscanf(fd, "%d", &n);
+  char buf[31];
+  int b;
+  fscanf(fd, "%s %d", buf, &b);
 
-  int buf[100][100] = {0};
-  int r, c;
-  for (int i = 0; i < n; i++) {
-    fscanf(fd, "%d %d", &r, &c);
-    r -= 1;
-    c -= 1;
-    for (int j = r; j < r + 10; j++) {
-      for (int k = c; k < c + 10; k++) {
-        if (buf[j][k]) continue;
-        buf[j][k]++;
-      }
-    }
+  int size = 0;
+  for (int i = 0; i < 31; i++) {
+    if (buf[i] == '\0') break;
+    size++;
   }
 
   int s = 0;
-  for (int i = 0; i < 100; i++) {
-    for (int j = 0; j < 100; j++) {
-      s += buf[i][j];
+  int e = size - 1;
+  for (int i = 0; i < size; i++) {
+    if (buf[i] >= 'A' && buf[i] <= 'Z') {
+      s += (buf[i] - 55) * (int)pow(b, e);
+      e--;
+      continue;
     }
+    s += (buf[i] - 48) * (int)pow(b, e);
+    e--;
   }
 
   printf("%d", s);
