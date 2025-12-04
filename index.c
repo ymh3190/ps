d32eb86ffc613be98885feb212b0a915961d988b
ymh3190 <ymh3190@gmail.com>
2025-12-04 15:16:46 +0900

    10798

--- index.c
+++ index.c
@@ -4,29 +4,17 @@ int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  int n, m;
-  fscanf(fd, "%d %d", &n, &m);
-
-  int a[n][m];
-  int b[n][m];
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      fscanf(fd, "%d", &a[i][j]);
-    }
-  }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      fscanf(fd, "%d", &b[i][j]);
-    }
+  char buf[5][16] = {0};
+  for (int i = 0; i < 5; i++) {
+    fscanf(fd, "%s", buf[i]);
   }
 
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      printf("%d ", a[i][j] + b[i][j]);
+  for (int j = 0; j < 15; j++) {
+    for (int i = 0; i < 5; i++) {
+      if (buf[i][j] == '\0') continue;
+      printf("%c", buf[i][j]);
     }
-    printf("\n");
   }
-
   fclose(fd);
   return 0;
 }
