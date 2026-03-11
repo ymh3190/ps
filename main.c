b025f9f7eb61df4fc71104f3c0285ab7b0c1aa72
ymh3190 <ymh3190@gmail.com>
2026-03-11 15:43:26 +0900

    24265

--- main.c
+++ main.c
@@ -4,32 +4,10 @@ int main() {
   FILE* fd;
   fd = fopen("input.txt", "r");
 
-  int buf[3];
-  for (int i = 0; i < 3; i++) {
-    fscanf(fd, "%d", &buf[i]);
-  }
+  int n;
+  fscanf(fd, "%d", &n);
 
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      if (j == i) continue;
-      if (buf[j] > buf[i]) {
-        int t = buf[j];
-        buf[j] = buf[i];
-        buf[i] = t;
-      }
-    }
-  }
-
-  int a, b, c;
-  a = buf[0];
-  b = buf[1];
-  c = buf[2];
-
-  if (c >= a + b) {
-    printf("%d", a + b + (a + b - 1));
-  } else {
-    printf("%d", a + b + c);
-  }
+  printf("%ld\n2", (long int)(n - 1) * n / 2);
 
   fclose(fd);
   return 0;
